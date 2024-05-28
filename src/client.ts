0cc5f883d248c3914eff5fb92cacd00525f63d38
coreybriscoe <coreybriscoe@gmail.com>
2024-05-27 20:52:30 -0400

    feat: Add default navigation buttons to client. Add an exclusion method

--- src/client.ts
+++ src/client.ts
@@ -2,6 +2,10 @@ import { Client, Collection, GatewayIntentBits } from 'discord.js';
 import { RendezvousSlashCommand } from './rendezvous/rendezvousCommand.js';
 import { OutcomeTypeConstraint } from './rendezvous/outcome.js';
 import CustomButton from './buttons/architecture/CustomButton.js';
+import firstButton from './buttons/first.js';
+import lastButton from './buttons/last.js';
+import nextButton from './buttons/next.js';
+import previousButton from './buttons/previous.js';
 
 // Type alias for RendezvousSlashCommand with unknown generic parameters.
 type RdvsSlashCommandAlias = RendezvousSlashCommand<OutcomeTypeConstraint, unknown, unknown>;
@@ -18,21 +22,34 @@ type ButtonCollectionPair = {
 
 export class RendezvousClient extends Client {
     private static instance: RendezvousClient;
-    private commands: Collection<string, RdvsSlashCommandAlias>;
-    private buttons: Collection<string, CustomButton>;
-    private static intents: GatewayIntentBits[];
+    private commands: Collection<string, RdvsSlashCommandAlias> = new Collection();
+    private buttons: Collection<string, CustomButton> = new Collection();
+    private static intents: GatewayIntentBits[] = [];
+    private static addBuiltInButtons: boolean = true;
 
     private constructor() {
         super({
             intents: RendezvousClient.intents,
         });
-        this.commands = new Collection();
-        this.buttons = new Collection();
+        if (RendezvousClient.addBuiltInButtons) {
+            this.buttons = new Collection([
+                [firstButton.getCustomId(), firstButton],
+                [lastButton.getCustomId(), lastButton],
+                [nextButton.getCustomId(), nextButton],
+                [previousButton.getCustomId(), previousButton],
+            ]);
+        }
         RendezvousClient.instance = this;
     }
 
     public static addIntents(intents: GatewayIntentBits[]): void {
         RendezvousClient.intents = RendezvousClient.intents.concat(intents);
+    }
+
+    // TODO: This behavior better suited in e.g. a builder pattern
+    public static excludeBuiltInButtons(): void {
+        if (RendezvousClient.instance) throw new Error('Error in client.ts: This method must be called before singleton instantiation.');
+        RendezvousClient.addBuiltInButtons = false;
     }
 
     public addCommands(commands: SlashCommandCollectionPair[]): void {
